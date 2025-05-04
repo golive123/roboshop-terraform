129bddad70dc041eab834fed71000c5549b7eb62
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-04 21:03:19 +0530

    terraform vm file

--- learn-terraform/01-resource/main.tf
+++ learn-terraform/01-resource/main.tf
@@ -14,8 +14,7 @@ resource "azurerm_virtual_machine" "test" {
   delete_os_disk_on_termination = true
 
   storage_image_reference {
-    id = "/subscriptions/323379f3-3beb-4865-821e-0fff68e4d4ca/resourceGroups/project-setup-1/providers/Microsoft.Compute/images/local-devops-practice"
-  }
+    id = "/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Compute/images/imageserver-golive"
   storage_os_disk {
     name              = "test-terraform-disk"
     caching           = "ReadWrite"
