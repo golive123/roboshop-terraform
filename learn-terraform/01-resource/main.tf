ebf040e6dc94aa3fdb0b2e96be58d9e16c6a4153
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-04 21:00:29 +0530

    terraform vm file

--- learn-terraform/01-resource/main.tf
+++ learn-terraform/01-resource/main.tf
@@ -3,27 +3,31 @@ provider "azurerm" {
   subscription_id = "a9bc3c93-b459-4ffb-8364-38ff9554f652"
 }
 
-resource "azurerm_linux_virtual_machine" "test" {
-  name                = "terraform-test"
-  location            = "UK West"
-  resource_group_name = "golive"
-  size                = "Standard_B2s"
-  admin_username      = "devops18"
-  admin_password      = "Passw0rd@1234"
-  network_interface_ids = ["/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive"]
-  disable_password_authentication = false
 
-  source_image_id = "/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Compute/images/imageserver-golive"
+resource "azurerm_virtual_machine" "test" {
+  name                  = "test-terraform"
+  location              = "UK West"
+  resource_group_name   = "golive"
+  network_interface_ids = ["/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Network/virtualNetworks/golive-vnet"]
+  vm_size               = "Standard_B2s"
 
-  os_disk {
-    name                 = "terraform-disk"
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+  delete_os_disk_on_termination = true
+
+  storage_image_reference {
+    id = "/subscriptions/323379f3-3beb-4865-821e-0fff68e4d4ca/resourceGroups/project-setup-1/providers/Microsoft.Compute/images/local-devops-practice"
   }
-
-  computer_name = "terraform-test"
-
-  tags = {
-    environment = "staging"
+  storage_os_disk {
+    name              = "test-terraform-disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "test-terraform"
+    admin_username = "devops18"
+    admin_password = "Passw0rd@1234"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
   }
 }
