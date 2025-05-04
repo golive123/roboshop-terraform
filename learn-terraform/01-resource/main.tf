12432a503403117410d62dc6481236454f168e6b
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-04 20:51:13 +0530

    terraform vm file

--- learn-terraform/01-resource/main.tf
+++ learn-terraform/01-resource/main.tf
@@ -1,37 +1,30 @@
 provider "azurerm" {
   features {}
   subscription_id = "a9bc3c93-b459-4ffb-8364-38ff9554f652"
-  }
-resource "azurerm_virtual_machine" "test" {
-  name                  = "terraform-test"
-  location              = "UK West"
-  resource_group_name   = "golive"
-  network_interface_ids = ["/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Network/virtualNetworks/golive-vnet"]
-  vm_size               = "Standard_B2s"
+}
 
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  delete_os_disk_on_termination = true
+resource "azurerm_linux_virtual_machine" "test" {
+  name                = "terraform-test"
+  location            = "UK West"
+  resource_group_name = "golive"
+  size                = "Standard_B2s"
+  admin_username      = "devops18"
+  admin_password      = "Passw0rd@1234"
+  network_interface_ids = [
+    "/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Network/networkInterfaces/golive-nic"
+  ]
+  disable_password_authentication = false
 
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
+  source_image_id = "/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Compute/images/imageserver-golive"
 
-  storage_image_reference {
-    id= "/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Compute/images/imageserver-golive"
+  os_disk {
+    name                 = "terraform-disk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
-  storage_os_disk {
-    name              = "terraform-disk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "terraform-test"
-    admin_username = "devops18"
-    admin_password = "Passw0rd@1234"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
+
+  computer_name = "terraform-test"
+
   tags = {
     environment = "staging"
   }
