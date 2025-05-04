d14e27ddb72d37ed57a0409e1f4dc4bb80df99a8
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-04 21:17:25 +0530

    terraform vm file

--- learn-terraform/01-resource/main.tf
+++ learn-terraform/01-resource/main.tf
@@ -3,43 +3,14 @@ provider "azurerm" {
   subscription_id = "a9bc3c93-b459-4ffb-8364-38ff9554f652"
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "golive"
-  location = "UK West"
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "golive-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "golive-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_network_interface" "nic" {
-  name                = "golive-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
 resource "azurerm_virtual_machine" "test" {
   name                  = "test-vm"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = "Standard_B2s"
+  location              = "UK West"
+  resource_group_name   = "golive"
+  network_interface_ids = [
+    "/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Network/networkInterfaces/golive-nic"
+  ]
+  vm_size = "Standard_B2s"
 
   storage_image_reference {
     id = "/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Compute/images/imageserver-golive"
