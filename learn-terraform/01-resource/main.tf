3cea88176d90e9237f2b4e6f77e37e3714600a9c
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-05 10:33:39 +0530

    terraform vm file

--- learn-terraform/01-resource/main.tf
+++ learn-terraform/01-resource/main.tf
@@ -3,37 +3,31 @@ provider "azurerm" {
   subscription_id = "a9bc3c93-b459-4ffb-8364-38ff9554f652"
 }
 
-resource "azurerm_virtual_machine" "test" {
-  name                  = "test-vm"
+
+resource "azurerm_virtual_machine" "terraform" {
+  name                  = "terraform-vm"
   location              = "UK West"
   resource_group_name   = "golive"
-  network_interface_ids = [
-    "/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Network/networkInterfaces/golive-nic"
-  ]
-  vm_size = "Standard_B2s"
+  network_interface_ids = ["/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Network/networkInterfaces/workstation758"]
+  vm_size               = "Standard_B2s"
+
+  delete_os_disk_on_termination = true
 
   storage_image_reference {
-    id = "/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Compute/images/imageserver-golive"
+    id = "/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Compute/images/terraform-terraform-image"
   }
-
   storage_os_disk {
-    name              = "test-vm-disk"
+    name              = "terraform-vm-disk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
-
   os_profile {
-    computer_name  = "test-vm"
+    computer_name  = "terraform-vm"
     admin_username = "devops18"
     admin_password = "Passw0rd@1234"
   }
-
   os_profile_linux_config {
     disable_password_authentication = false
-  }
-
-  tags = {
-    environment = "staging"
   }
 }
