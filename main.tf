e856c433650359f2c50a5d0e66c4841e53cf8ed4
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-05 18:41:13 +0530

    terraform vm file

--- main.tf
+++ main.tf
@@ -16,21 +16,21 @@ resource "azurerm_network_interface" "frontend" {
   resource_group_name   = "golive"
 
   ip_configuration {
-  name                          = "frontend"
-  subnet_id                     = "/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Network/virtualNetworks/golive-vnet/subnets/default"
-  private_ip_address_allocation = "Dynamic"
-}
+    name                          = "frontend"
+    subnet_id                     = "/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Network/virtualNetworks/golive-vnet/subnets/default"
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 resource  "azurerm_virtual_machine" "frontend" {
   name                  = "frontend-vm"
   location              = "UK West"
   resource_group_name   = "golive"
-  network_interface_ids = ["azure_network_interface.frontend.id"]
+  network_interface_ids = [azurerm_network_interface.frontend.id]
   vm_size               = "Standard_B2s"
   delete_os_disk_on_termination = true
 
   storage_image_reference {
-    id = "/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Compute/images/terraform-frontend-image"
+    id = "/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Compute/images/terraform-test-image"
   }
   storage_os_disk {
     name              = "frontend-vm-disk"
@@ -39,7 +39,7 @@ resource  "azurerm_virtual_machine" "frontend" {
     managed_disk_type = "Standard_LRS"
   }
   os_profile {
-    computer_name  = "frontend"
+    computer_name  = "frontend-vm"
     admin_username = "devops18"
     admin_password = "Passw0rd@1234"
   }
