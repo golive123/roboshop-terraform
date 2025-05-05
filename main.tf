4e115b674f5a2da054986aada946c387b10f753d
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-05 18:21:01 +0530

    terraform vm file

--- main.tf
+++ main.tf
@@ -7,16 +7,26 @@ resource "azurerm_public_ip" "frontend" {
   name                = "frontend"
   location              = "UK West"
   resource_group_name   = "golive"
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
-resource "azurerm_virtual_machine" "frontend" {
+resource "azurerm_network_interface" "frontend" {
+  name                = "frontend-nic"
+  location              = "UK West"
+  resource_group_name   = "golive"
+
+  ip_configuration {
+  name                          = "frontend"
+  subnet_id                     = "/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Network/virtualNetworks/golive-vnet/subnets/default"
+  private_ip_address_allocation = "Dynamic"
+}
+}
+resource  "azurerm_virtual_machine" "frontend" {
   name                  = "frontend-vm"
   location              = "UK West"
   resource_group_name   = "golive"
-  network_interface_ids = ["/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Network/networkInterfaces/workstation758"]
+  network_interface_ids = ["azure_network_interface.frontend.id"]
   vm_size               = "Standard_B2s"
-
   delete_os_disk_on_termination = true
 
   storage_image_reference {
@@ -29,17 +39,11 @@ resource "azurerm_virtual_machine" "frontend" {
     managed_disk_type = "Standard_LRS"
   }
   os_profile {
-    computer_name  = "frontend-vm"
+    computer_name  = "frontend"
     admin_username = "devops18"
     admin_password = "Passw0rd@1234"
   }
-
   os_profile_linux_config {
     disable_password_authentication = false
-  }
-  data "azurerm_dns_a_record" "frontend" {
-    name                = "frontend-dev"
-    zone_name           = "obicard.com"
-    resource_group_name = "golive"
   }
 }
