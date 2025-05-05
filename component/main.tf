555b433882988a23a0da8ecc61986697ee85b211
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-05 23:55:12 +0530

    terraform vm file

--- component/main.tf
+++ component/main.tf
@@ -49,4 +49,17 @@ resource "azurerm_virtual_machine" "vm" {
   os_profile_linux_config {
     disable_password_authentication = false
   }
+  connection {
+    type = "ssh"
+    user = "devops18"
+    password = "Passw0rd@1234"
+    host     = "azurerm_network_interface.privateip.id"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo dnf install python3.12 python3.12-pip-y",
+      "sudo pip3.12 install ansible"
+    ]
+  }
 }
+
