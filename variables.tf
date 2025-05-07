6b309092473f65ddfcb33a40436a5e4cdf13f84f
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-07 18:56:34 +0530

    terraform vm file

--- variables.tf
+++ variables.tf
@@ -17,3 +17,24 @@ variable "rg_name" {
 variable "storage_image_reference_id" {
   default = "/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Compute/images/terraform-test-image"
 }
+
+variable "databases" {
+  default = {
+    mongodb = {}
+    rabbitmq = {}
+    mysql    = {}
+    redis    = {}
+  }
+}
+
+variable "applications" {
+  default = {
+    catalogue = {}
+    cart = {}
+    user = {}
+    payment = {}
+    shipping = {}
+    frontend = {}
+  }
+
+ }
