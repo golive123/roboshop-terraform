cb47dca0c1211edad57323b1257227b4b76b8f44
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-07 22:43:08 +0530

    terraform vm file

--- component/main.tf
+++ component/main.tf
@@ -31,9 +31,9 @@ resource "azurerm_network_security_group" "nsg" {
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "Tcp"
+    protocol                   = "*"
     source_port_range          = "*"
-    destination_port_range     = "22"
+    destination_port_range     = "*"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
