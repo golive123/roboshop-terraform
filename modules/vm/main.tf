2a832ef9b0fe87aff89d8aa46a877126eebdb8b7
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-10 20:46:30 +0530

    terraform vm file

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -2,7 +2,7 @@
 resource "azurerm_public_ip" "publicip" {
   name                = var.name
   location            = var.location
-  resource_group_name = var.name
+  resource_group_name = var.rg_name
   allocation_method   = "Static"
 }
 
