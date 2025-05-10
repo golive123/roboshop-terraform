4b522dead510b48f9ee2312ae71a8a78d9669741
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-10 21:53:53 +0530

    terraform vm file

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ module "databases" {
   source                     = "./modules/vm"
   ip_configuration_subnet_id = var.ip_configuration_subnet_id
   name                       = each.key
-  rg_name                    = module.resource-group[each.value["rgname"]].name
-  rg_location                = module.resource-group[each.value["rgname"]].location
+  rg_name                    = module.resource-group[each.value["main"]].name
+  rg_location                = module.resource-group[each.value["main"]].location
   storage_image_reference_id = var.storage_image_reference_id
   zone_name                  = var.zone_name
   network_security_group_id  = var.network_security_group_id
