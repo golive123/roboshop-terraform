77614b6e77dc8c64ffe8df91ad48e6d40cc18441
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-11 12:57:55 +0530

    terraform vm file

--- main.tf
+++ main.tf
@@ -17,19 +17,19 @@ module "databases" {
   network_security_group_id  = var.network_security_group_id
   dns_record_rg_name         = var.dns_record_rg_name
 }
-#
-# module "applications" {
-#   depends_on                 = [ module.databases ]
-#   for_each                   = var.applications
-#   source                     = "./modules/vm"
-#   name                       = each.key
-#   location                   = var.location
-#   rg_name                    = var.rg_name
-#   storage_image_reference_id = var.storage_image_reference_id
-#   ip_configuration_subnet_id = var.ip_configuration_subnet_id
-#   network_security_group_id  =  var.network_security_group_id
-#   zone_name                  = var.zone_name
-# }
+
+module "applications" {
+  depends_on                 = [ module.databases ]
+  for_each                   = var.applications
+  source                     = "./modules/vm"
+  name                       = each.key
+  rg_name                    = module.resource-group[each.value["rgname"]].name
+  rg_location                = module.resource-group[each.value["rgname"]].location
+  storage_image_reference_id = var.storage_image_reference_id
+  ip_configuration_subnet_id = var.ip_configuration_subnet_id
+  network_security_group_id  =  var.network_security_group_id
+  zone_name                  = var.zone_name
+}
 
 
 
