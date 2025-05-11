2853d1cb451cf9bd51eabb24d198d0b534683bfc
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-11 12:17:54 +0530

    terraform vm file

--- variables.tf
+++ variables.tf
@@ -5,55 +5,7 @@ variable "network_security_group_id" {}
 variable "databases" {}
 variable "applications" {}
 variable "rg_name" {}
-variable "rg_location" {}
+#variable "rg_location" {}
 variable "env" {}
 variable "dns_record_rg_name" {}
 
-
-# variable "ip_configuration_subnet_id" {
-#   default = "/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Network/virtualNetworks/golive-vnet/subnets/default"
-# }
-#
-# variable "zone_name" {
-#   default = "yourtherapist.in"
-# }
-#
-# ## added these details in data block,so in main.tf file change the location details as #location location=data.azurerm_resource_group.rg.location
-# # and also remove information from component/main.tf and also remove location from variables section as well
-# variable "location" {
-#   default = "UK West"
-# }
-#
-# variable "rg_name" {
-#   default = "golive"
-# }
-#
-# variable "storage_image_reference_id" {
-#   default = "/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Compute/images/terraform-test-image"
-# }
-#
-# variable "network_security_group_id" {
-#   default = "/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Network/networkSecurityGroups/allow-all"
-# }
-#
-# variable "databases" {
-#   default = {
-#     mongodb = {}
-#     rabbitmq = {}
-#     mysql    = {}
-#     redis    = {}
-#   }
-# }
-#
-# variable "applications" {
-#   default = {
-#     frontend-robo = {}
-#     catalogue = {}
-#     user = {}
-#     cart = {}
-#     shipping = {}
-#     payment = {}
-#   }
-# }
-#
-#
