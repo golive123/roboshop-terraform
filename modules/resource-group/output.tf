3de6f2c2cabb2a6705b3840299a40945e7c083a5
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-11 12:45:44 +0530

    terraform vm file

--- modules/resource-group/output.tf
+++ modules/resource-group/output.tf
@@ -1,9 +1,9 @@
 output "name" {
-  value = "azurerm_resource_group.main.name"
+  value = azurerm_resource_group.main.name
 }
 output "id" {
-  value = "azurerm_resource_group.main.id"
+  value = azurerm_resource_group.main.id
 }
 output "location" {
-  value = "azurerm_resource_group.main.location"
+  value = azurerm_resource_group.main.location
 }
