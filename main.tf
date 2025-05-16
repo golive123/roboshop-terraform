d505942bb7e8b21b65119323194fcb2f2d7688f9
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-16 20:18:01 +0530

    terraform vm file

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ module "databases" {
   network_security_group_id  = var.network_security_group_id
   dns_record_rg_name         = var.dns_record_rg_name
   token                      = var.token
+  env                        = var.env
 }
 
 module "applications" {
@@ -32,6 +33,7 @@ module "applications" {
   zone_name                  = var.zone_name
   dns_record_rg_name         = var.dns_record_rg_name
   token                      = var.token
+  env                        = var.env
 }
 
 
