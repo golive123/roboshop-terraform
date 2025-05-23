e845abc7e37640fbc65a6b2c5d62bea8c805bebd
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-23 16:37:18 +0530

    commit this

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -4,16 +4,16 @@ resource "azurerm_kubernetes_cluster" "main" {
   resource_group_name = var.rg_name
   dns_prefix          = "roboshop"
 
-  aci_connector_linux {
-    subnet_name = "/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Network/virtualNetworks/golive-vnet/subnets/default"
-  }
-
   default_node_pool {
     name       = "default"
     node_count = 1
     vm_size    = "standard_a2_v2"
+    vnet_subnet_id = "/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Network/virtualNetworks/golive-vnet/subnets/default"
   }
 
+  aci_connector_linux {
+    subnet_name = "/subscriptions/a9bc3c93-b459-4ffb-8364-38ff9554f652/resourceGroups/golive/providers/Microsoft.Network/virtualNetworks/golive-vnet/subnets/default"
+  }
 
 
   identity {
