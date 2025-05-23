0e2b9ec9213b623dac4a6a6b2773b18293847503
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-23 23:59:47 +0530

    commit this

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -1,8 +1,9 @@
 
 resource "null_resource" "kubeconfig" {
-  command = "az aks get-credentials --name ${var.name} --resource-group ${var.rg_name} --overwrite-existing"
+  provisioner "local-exec" {
+    command = "az aks get-credentials --name ${var.name} --resource-group ${var.rg_name} --overwrite-existing"
+  }
 }
-
 resource "helm_release" "external-secrets" {
   depends_on = [
     null_resource.kubeconfig
