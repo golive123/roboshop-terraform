3be38b9f03af91aa0388d176b36feb4ea8b6fcd8
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-25 21:51:28 +0530

    commit this

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -16,7 +16,7 @@ resource "helm_release" "external-secrets" {
   name             = "external-secrets"
   repository       = "https://charts.external-secrets.io"
   chart            = "external-secrets"
-  namespace        = "dev"
+  namespace        = "devops"
   create_namespace = true
   set {
     name  = "installCRDs"
