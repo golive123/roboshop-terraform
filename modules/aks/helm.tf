b1d3a41beaab54f402e8cac1da84af4167b93aa5
UdayaCharagundla <uday.devops18@gmail.com>
2025-05-25 19:23:42 +0530

    commit this

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -1,30 +1,9 @@
-#
-# provider "kubernetes" {
-#   config_path = "~/.kube/config"
-# }
-#
-# provider "helm" {
-#   kubernetes {
-#     config_path = "~/.kube/config"
-#   }
-# }
+
 resource "null_resource" "kubeconfig" {
   provisioner "local-exec" {
   command = "az aks get-credentials --name ${var.name} --resource-group ${var.rg_name} --overwrite-existing --file kubeconfig-${var.env}.yaml"
   }
 }
-
-# provider "helm" {
-#   kubernetes {
-#     config_path = "${path.module}/kubeconfig-${var.env}.yaml"
-#   }
-# }
-
-# resource "null_resource" "kubeconfig" {
-#   provisioner "local-exec" {
-#     command = "az aks get-credentials --name ${var.name} --resource-group ${var.rg_name} --overwrite-existing"
-#   }
-# }
 resource "helm_release" "external-secrets" {
   depends_on = [
     null_resource.kubeconfig
