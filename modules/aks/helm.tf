17f3f5b7544ee4234ca8b5872f54b4901d20ab46
UdayaCharagundla <uday.devops18@gmail.com>
2025-06-23 19:08:40 +0530

    commit ..

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -16,12 +16,13 @@ resource "helm_release" "external-secrets" {
   chart            = "external-secrets"
   namespace        = "devops"
   create_namespace = true
-  set =
+  set = [
     {
       name  = "installCRDs"
       value = "true"
     }
-   }
+  ]
+}
 
 resource "null_resource" "external-secrets-secret-store1" {
   depends_on = [
